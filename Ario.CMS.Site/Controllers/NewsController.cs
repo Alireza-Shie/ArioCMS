cc5a1b0c083d105af07a30624a87bc741c5c2bae
Alireza-Shie <alireza.shie86@gmail.com>
2021-01-18 22:28:46 +0330

    Update-Project

---
+++ Ario.CMS.Site/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ario.CMS.Data.Context.UnitOfWork;
+
+namespace Ario.CMS.Site.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly Context db = new Context();
+
+        [Route("Archive")]
+        public IActionResult Archive()
+        {
+            var pages = db.Page.GetAllPages();
+            return View(pages);
+        }
+    }
+}
